dc46d4192315714518a739777a3e2351a2195823
Thomas Horan <t.v.horan@gmail.com>
2018-07-21 20:02:38 +0100

    Added then removed logo from roasters until I find a better way of doing it, ie uploading an image.
    Added string representation of roaster

--- coffee/models.py
+++ coffee/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Roaster(models.Model):
     name = models.CharField(max_length=150)
     url = models.URLField()
+
+    def __str__(self):
+        return self.name
