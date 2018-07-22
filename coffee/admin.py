67265fc2870c48805f7c0d57a52fb676d0e8e4e3
Thomas Horan <t.v.horan@gmail.com>
2018-07-22 19:40:00 +0100

    Added beans and taggit

--- coffee/admin.py
+++ coffee/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from .models import Roaster
+from .models import Roaster, Bean
 
-admin.site.register(Roaster)
+
+class BeansInline(admin.StackedInline):
+    model = Bean
+
+
+@admin.register(Roaster)
+class RoasterAdmin(admin.ModelAdmin):
+    inlines = [BeansInline]
+
+
+admin.site.register(Bean)
--- coffee/models.py
+++ coffee/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 
 class Roaster(models.Model):
@@ -7,3 +8,25 @@ class Roaster(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Bean(models.Model):
+    name = models.CharField(max_length=100)
+    roaster = models.ForeignKey(Roaster, on_delete=models.CASCADE)
+    country_of_origin = models.CharField(max_length=100)
+    region = models.CharField(max_length=100, null=True)
+    varietal = models.CharField(max_length=50, null=True)
+    processing_method = models.CharField(max_length=100, null=True)
+    tasting_notes = TaggableManager()
+
+    ROAST_LEVELS = (
+        ('l', 'Light'),
+        ('m', 'Medium'),
+        ('md', 'Medium Dark'),
+        ('d', 'Dark'),
+    )
+
+    roast_level = models.CharField(max_length=2, choices=ROAST_LEVELS, help_text='Roast Level')
+
+    def __str__(self):
+        return self.name + " by " + self.roaster.name
