ae93ff0a4dbda9f9d278bc2734cfff1f9001f8da
Thomas Horan <t.v.horan@gmail.com>
2018-07-24 19:42:15 +0100

    You can now view a list of beans.

--- coffee/urls.py
+++ coffee/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.RoasterListView.as_view(), name='roasters'),
     path('roaster/<int:pk>', views.RoasterDetailView.as_view(), name='roaster-detail'),
+    path('beans', views.BeansListView.as_view(), name='beans'),
 ]
--- coffee/views.py
+++ coffee/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from django.views import generic
 
-from coffee.models import Roaster
+from coffee.models import Roaster, Bean
 
 
 class RoasterListView(generic.ListView):
@@ -11,3 +11,8 @@ class RoasterListView(generic.ListView):
 
 class RoasterDetailView(generic.DetailView):
     model = Roaster
+
+
+class BeansListView(generic.ListView):
+    model = Bean
+
